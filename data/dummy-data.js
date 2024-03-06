91b63936bc3ba81acf6ae0ac4b940fe9f3fbd782
Rory Magee <roryemagee@gmail.com>
2024-03-06 13:22:29 -0700

    Add temporary dummy data file for testing individual routes

---
+++ data/dummy-data.js
@@ -0,0 +1,107 @@
+const DUMMY_DATA = {
+  users: [
+    {
+      id: 'u1',
+      userName: 'SaveJarvis',
+      details: {
+        firstName: 'Jarvis',
+        lastName: 'Considine',
+        email: 'jarvis@considine.com',
+        password: 'test123!',
+        created: Date.now()
+      },
+      settings: {},
+      posts: [],
+      history: []
+    },
+    {
+      id: 'u2',
+      userName: 'BobSmith',
+      details: {
+        firstName: 'Bob',
+        lastName: 'Smith',
+        email: 'bob@smith.com',
+        password: 'test123!',
+        created: Date.now()
+      },
+      settings: {},
+      posts: [],
+      history: []
+    },
+    {
+      id: 'u3',
+      userName: 'JaneAnderson',
+      details: {
+        firstName: 'Jane',
+        lastName: 'Anderson',
+        email: 'jane@anderson.com',
+        password: 'test123!',
+        created: Date.now()
+      },
+      settings: {},
+      posts: [],
+      history: []
+    },
+    {
+      id: 'u4',
+      userName: 'EricLarson',
+      details: {
+        firstName: 'Eric',
+        lastName: 'Larson',
+        email: 'eric@larson.com',
+        password: 'test123!',
+        created: Date.now()
+      },
+      settings: {},
+      posts: [],
+      history: []
+    },
+    {
+      id: 'u5',
+      userName: 'DavidBergman',
+      details: {
+        firstName: 'David',
+        lastName: 'Bergman',
+        email: 'david@bergman.com',
+        password: 'test123!',
+        created: Date.now()
+      },
+      settings: {},
+      posts: [],
+      history: []
+    },
+    {
+      id: 'u6',
+      userName: 'BrigidMagee',
+      details: {
+        firstName: 'Brigid',
+        lastName: 'Magee',
+        email: 'brigid@magee.com',
+        password: 'test123!',
+        created: Date.now()
+      },
+      settings: {},
+      posts: [],
+      history: []
+    },
+    {
+      id: 'u7',
+      userName: 'FlashKolorkin',
+      details: {
+        firstName: 'Flash',
+        lastName: 'Kolorkin',
+        email: 'flash@kolorkin.com',
+        password: 'test123!',
+        created: Date.now()
+      },
+      settings: {},
+      posts: [],
+      history: []
+    }
+  ],
+  general: {
+    things: 'stuff'
+  } 
+}
+
+module.exports = DUMMY_DATA;
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,29 +1,34 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
+const HttpError = require('../model/http-error');
 
 const router = express.Router();
 
-const DUMMY_USERS = [
-  {
-    id: 'u1',
-    userName: 'SaveJarvis',
-    details: {
-      firstName: 'Jarvis',
-      lastName: 'Considine',
-      email: 'jarvis@considine.com',
-      password: 'test123!',
-      created: Date.now()
-    },
-    settings: {},
-    posts: [],
-    history: []
-  }
-]
+const DUMMY_DATA = require('../data/dummy-data');
+
+// const DUMMY_DATA = {
+//   users: [
+//     {
+//       id: 'u1',
+//       userName: 'SaveJarvis',
+//       details: {
+//         firstName: 'Jarvis',
+//         lastName: 'Considine',
+//         email: 'jarvis@considine.com',
+//         password: 'test123!',
+//         created: Date.now()
+//       },
+//       settings: {},
+//       posts: [],
+//       history: []
+//     }
+//   ]
+// }
+
 
 router.get('/user/:uid', (req, res, next) => {
   const userID = req.params.uid;
-  const user = DUMMY_USERS.find(user => user.id === userID)
+  const user = DUMMY_DATA.users.find(user => user.id === userID)
   console.log('GET Request in Users');
  
 
@@ -34,7 +39,5 @@ router.get('/user/:uid', (req, res, next) => {
 
   res.json({ user });
 })
-
-
 
 module.exports = router;
