856d1696e6d8e2b550196f4588c1676ec702f239
Rory Magee <roryemagee@gmail.com>
2024-03-06 12:39:32 -0700

    Add HttpError model and basic 404 error handling

--- app.js
+++ app.js
@@ -7,4 +7,13 @@ const app = express();
 
 app.use('/api/users', usersRoutes);
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error has occurred.'})
+});
+
 app.listen(3000);
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_USERS = [
@@ -23,7 +25,16 @@ router.get('/user/:uid', (req, res, next) => {
   const userID = req.params.uid;
   const user = DUMMY_USERS.find(user => user.id === userID)
   console.log('GET Request in Users');
+ 
+
+  if (!user) {
+    const error = new HttpError('Could not find a user with the given userID', 404);
+    return next(error);
+  }
+
   res.json({ user });
 })
+
+
 
 module.exports = router;
