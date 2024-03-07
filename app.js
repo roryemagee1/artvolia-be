c3d9464a500e77d53c391729ce8e3a4aa9022517
Rory Magee <roryemagee@gmail.com>
2024-03-06 19:09:40 -0700

    Add 404 error route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
@@ -9,6 +10,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find the requested route.', 404);
+  next(error);
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
