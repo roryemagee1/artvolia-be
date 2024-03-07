15638f7a96666a7b6571518293cbfdcfe6939740
Rory Magee <roryemagee@gmail.com>
2024-03-07 16:28:59 -0700

    Add list of routes to build in user-routes.js

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -6,9 +6,13 @@ const router = express.Router();
 const usersControllers = require('../controllers/users-controllers');
 
 // User & Profile Routes
-router.get('users/all', usersControllers.getAllUsers);
+router.get('/users', usersControllers.getAllUsers);
 
-router.get('users/user/:uid', usersControllers.getUserByID);
+router.get('/users/:uid', usersControllers.getUserByID);
+
+// router.get('/profile/:uid); // Will get a certain user's profile.
+// router.patch('/profile/:uid/update); // Will allow a user to update their profile.
+// router.get('/public/profile/:uid); // Will get a certain user's public profile if it is available.
 
 // Auth Routes
 router.patch('/login', usersControllers.loginUserByID);
@@ -16,15 +20,37 @@ router.patch('/login', usersControllers.loginUserByID);
 router.post('/sign-up', usersControllers.signUpUser);
 
 // Feed Routes
-// router.get('/feed', 'function'); Will retrieve a list of all posts.
-// router.get('/feed?num=16&index=0'); Will retrieve the 16 posts found startings at index 0, which are the 16 most recent posts.
-// router.get('/feed?num=16&index=0/filter?'); Will give the user options for searches and filters.
-// router.get('public/feed', 'function'); Will retrieve a list of public only posts.
+// router.get('/feed'); // Will retrieve a list of all posts.
+// router.get('/feed?num=16&index=0'); // Will retrieve the 16 posts found startings at index 0, which are the 16 most recent posts.
+// router.get('/feed?num=16&index=0/filter?'); // Will give the user options for searches and filters.
+// router.get('/public/feed); // Will retrieve a list of public only posts.
 
 // Posts Routes
+// router.post('/posts/:uid/post); // Will create a post.
+// router.get('/posts/:uid/:pid); // Will return the user's post with that postID along with comment data, etc.
+// // router.put('/posts/:uid/:pid); // Will edit a post.
+// router.patch('/posts/:uid/:pid); // Will like a post.
+// router.delete('/posts/:uid/:pid); // Will delete a post.
+// router.patch('/posts/:uid/:pid/settings) // Will modify the settings for a specific post.
+
+// router.post('/posts/:uid/:pid/comment'); // Will create a new comment.
+// // router.get('/posts/:uid/:pid/:cid): // Will get a specific comment.
+// // router.put('/posts/:uid/:pid/:cid); // Will edit a comment.
+// router.patch('/posts/:uid/:pid/:cid'): // Will like a comment.
+// router.delete('/posts/:uid/:pid/:cid'): // Will delete a comment.
+
+// router.post('/posts/:uid/:pid/:cid/reply'): // Will create a new reply.
+// // router.get('/posts/:uid/:pid/:cid/:rid'): // Will get a specific reply.
+// // router.put('/posts/:uid/:pid/:cid/:rid'): // Will edit a reply.
+// router.patch('/posts/:uid/:pid/:cid/:rid'): // Will like a reply.
+// router.delete('/posts/:uid/:pid/:cid/:rid'): // Will delete a reply.
+// Note: the PUT route will be reserved for the possibility of editting text in the future.
 
 // Settings Routes
-router.delete('users/user/:uid', usersControllers.deleteUser);
+router.delete('/users/user/:uid', usersControllers.deleteUser); // This will be refactored to a new controller.
+
+// router.get('/settings/:uid'); // This will get the current user's settings.
+// router.patch('/settings/:uid/:option); // This will update the the setting specified at the end of the URL.
 
 
 
