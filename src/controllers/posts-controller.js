ed54488ab95d05a6ae882d3f127d87c463e460e2
Rory Magee <roryemagee@gmail.com>
2024-05-02 13:55:57 -0600

    Remove commented out code that is no longer relevant to the project

--- src/controllers/posts-controller.js
+++ src/controllers/posts-controller.js
@@ -8,15 +8,6 @@ const User = require('../models/user');
 
 const getPostsByUserId = async (req, res, next) => {
   const userID = req.params.uid;
-  
-  // let posts;
-  // try {
-  //   posts = await Post.find({ userID: userID });
-  // } catch(err) {
-  //   const error = new HttpError("Failed to fetch user's posts.", 500);
-  //   return next(error);
-  // }
-  // res.status(200).json({posts: posts.map(post => post.toObject({ getters:  true }))});
 
   let userWithPosts;
   try {
@@ -33,22 +24,6 @@ const getPostsByUserId = async (req, res, next) => {
 
   res.status(200).json({posts: userWithPosts.map(post => post.toObject({ getters:  true }))});
 }
-
-// const getPostsByUserId = (req, res, next) => {
-//   const userID = req.params.uid;
-//   const user = DUMMY_DATA.users.find(user => user.userID === userID);
-  
-//   if (!user) {
-//    return next(new HttpError("Could not find a user with the provided user ID.", 404));
-//   }
-
-//   if (!user.posts || user.posts.length === 0 ) {
-//     return next(new HttpError("Could not find any posts associated with provided user ID", 404));
-//   }
-
-//   res.status(200).json(user.posts);
-// }
-
 
 const getPostByUserId = async (req, res, next) => {
   const postID = req.params.pid;
@@ -68,28 +43,9 @@ const getPostByUserId = async (req, res, next) => {
   res.status(200).json({ post: post });
 }
 
-// const getPostByUserId = (req, res, next) => {
-//   const userID = req.params.uid;
-//   const postID = req.params.pid;
-//   const user = DUMMY_DATA.users.find(user => user.userID === userID);
-  
-//   if (!user) {
-//    return next(new HttpError("Could not find a user with the provided user ID.", 404));
-//   }
-
-//   const post = user.posts.find(post => post.postID === postID);
-
-//   if (!post) {
-//     return next(new HttpError("Could not find a post with the provided post ID.", 404));
-//    }
-
-//   res.status(200).json(post);
-// }
-
 const createPost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors);
     return next(new HttpError("Invalid inputs passed. Please check your data.", 422));
   }
 
@@ -136,38 +92,6 @@ const createPost = async (req, res, next) => {
   res.status(201).json({ post: createdPost});
 };
 
-// const createPost = (req, res, next) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     console.log(errors);
-//     return next(new HttpError("Invalid inputs passed. Please check your data.", 422));
-//   }
-
-//   const { postText, creator } = req.body;
-//   const userID = req.params.uid;
-//   let moddedUser = DUMMY_DATA.users.find(user => user.userID === userID);
-
-//   if (!moddedUser) {
-//     return next(new HttpError("Could not find a user with the provided user ID.", 404));
-//   }
-
-//   const createdPost = {
-//     postID: "p-" + uuid.v4(),
-//     postText,
-//     creator
-//   }
-
-//   moddedUser.posts.push(createdPost);
-//   DUMMY_DATA.users.map(user => {
-//     if (user.userID === userID) {
-//       return moddedUser;
-//     }
-//     return user;
-//   });
-  
-//   res.status(201).json({ post: createdPost});
-// };
-
 const updatePostByUserId = async (req, res, next) => {
   const postText = req.body.postText;
   const userID = req.params.uid;
@@ -198,40 +122,6 @@ const updatePostByUserId = async (req, res, next) => {
 
   res.status(200).json({ updatedPost: post.toObject({ getters: true }) });
 }
-
-// const updatePostByUserId = (req, res, next) => {
-//   const postText = req.body.postText;
-//   const userID = req.params.uid;
-//   const postID = req.params.pid;
-//   const user = DUMMY_DATA.users.find(user => user.userID === userID);
-  
-//   if (!user) {
-//    return next(new HttpError("Could not find a user with the provided user ID.", 404));
-//   }
-
-//   const post = user.posts.find(post => post.postID === postID);
-
-//   if (!post) {
-//     return next(new HttpError("Could not find a post with the provided post ID.", 404));
-//    }
-
-//    const moddedPosts = user.posts.map(post => {
-//     if (post.postID === postID) {
-//       post.postText = postText;
-//       return post;
-//     }
-//     return post;
-//    });
-//    DUMMY_DATA.users.map(user => {
-//     if (user.userID === userID) {
-//       user.posts = moddedPosts;
-//       return user;
-//     }
-//     return user;
-//   });
-
-//   res.status(201).json({ message: "Post updated!", moddedPosts});
-// }
 
 const deletePostByUserId = async (req, res, next) => {
   const postID = req.params.pid;
@@ -264,33 +154,6 @@ const deletePostByUserId = async (req, res, next) => {
 
   res.status(200).json({ message: "Post deleted!", post: post.toObject({ getters: true }) });
 }
-
-// const deletePostByUserId = (req, res, next) => {
-//   const userID = req.params.uid;
-//   const postID = req.params.pid;
-//   const user = DUMMY_DATA.users.find(user => user.userID === userID);
-  
-//   if (!user) {
-//    return next(new HttpError("Could not find a user with the provided user ID.", 404));
-//   }
-
-//   const post = user.posts.find(post => post.postID === postID);
-
-//   if (!post) {
-//     return next(new HttpError("Could not find a post with the provided post ID.", 404));
-//    }
-
-//    const moddedPosts = user.posts.filter(post => post.postID !== postID);
-//    DUMMY_DATA.users.map(user => {
-//     if (user.userID === userID) {
-//       user.posts = moddedPosts;
-//       return user;
-//     }
-//     return user;
-//   });
-
-//   res.status(200).json({ message: "Post deleted!", post});
-// }
 
 exports.getPostsByUserId = getPostsByUserId;
 exports.getPostByUserId = getPostByUserId;
--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -21,16 +21,6 @@ const getUsers = async (req, res, next) => {
   res.status(200).json({ users: users.map(user => user.toObject({ getters: true })) });
 }
 
-// const getUsers = (req, res, next) => {
-//   const users = DUMMY_DATA.users;
-  
-//   if (!users) {
-//     return next(new HttpError("Requested data not available.", 404));
-//   }
-
-//   res.status(200).json({users: users});
-// }
-
 const getUserById = (req, res, next) => {
   const userID = req.params.uid;
   const user = DUMMY_DATA.users.find(user => user.userID === userID);
@@ -87,26 +77,6 @@ const signup = async (req, res, next) => {
   res.status(201).json({ newUser: createdUser.toObject({ getters: true })});
 };
 
-// const signup = (req, res, next) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return next(new HttpError(`Invalid ${errors.errors[0].path} value.`, 422));
-//   }
-
-//   const { email, userName, firstName, lastName, password } = req.body;
-//   const user = DUMMY_DATA.users.find(user => user.userName === userName);
-
-//   if (user) {
-//     const error = new HttpError("The provided username is already in use.", 422);
-//     return next(error);
-//   }
-
-//   const newUser = new SignUp(email, userName, firstName, lastName, password);
-//   DUMMY_DATA.users.push(newUser);
-
-//   return res.status(201).json({ newUser: newUser });
-// };
-
 const login = async (req, res, next) => {
   const { userName, password } = req.body;
 
@@ -138,29 +108,6 @@ const login = async (req, res, next) => {
   });
 };
 
-// const login = (req, res, next) => {
-//   const { userName, password } = req.body;
-
-//   let user = DUMMY_DATA.users.find(user => user.userName === userName);
-
-//   if (!user || user.settings.information.password !== password) {
-//     const error = new HttpError("No user with that username and password combination was found.", 401);
-//     return next(error);
-//   }
-
-//     DUMMY_DATA.users.map(user => {
-//       if (user.userName === userName) {
-//         user.loggedIn = true;
-//         return user;
-//       }
-//       return user;
-//     });
-//     return res.status(200).json({ 
-//       message: "Login successful!",
-//       login: user.loggedIn
-//     });
-// };
-
 const logout = async (req, res, next) => {
   const { userName } = req.body;
 
@@ -186,31 +133,6 @@ const logout = async (req, res, next) => {
     loginStatus: existingUser.toObject({ getters: true }).loggedIn
   });
 };
-
-// const logout = (req, res, next) => {
-//   const { userName } = req.body;
-
-//   let user = DUMMY_DATA.users.find(user => user.userName === userName);
-
-//   if (!user ) {
-//     const error = new HttpError("An unknown error has occurred.", 404);
-//     return next(error);
-//   }
-
-//   user.loggedIn = false;
-
-//   DUMMY_DATA.users.map(user => {
-//     if (user.userName === userName) {
-//       user.loggedIn = false
-//       return user;
-//     }
-//     return user;
-//   });
-//   return res.status(200).json({ 
-//     message: "Logout successful!",
-//     login: user.loggedIn
-//     });
-// };
 
 exports.getUsers = getUsers;
 exports.getUserById = getUserById;
